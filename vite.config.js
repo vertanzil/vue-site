0437b44a23c034c49d56e8d8af6a2f6e85ad7a87
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2025-01-09 20:44:59 +0000

    Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
   base: '/vue-site/', // Replace with your GitHub repository name
